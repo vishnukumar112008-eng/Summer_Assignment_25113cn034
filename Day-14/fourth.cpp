b3743b5a99f820dc064a1ed34af4e46c7d929cf3
Vishnu kumar <vishnukumar112008@gmail.com>
2026-06-14 22:40:53 +0530

    new

--- Day-14/fourth.cpp
+++ Day-14/fourth.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 int main (){
@@ -13,6 +13,22 @@ int main (){
     cout << "duplicate elements are: ";
     for(int i = 0; i< n ; i++){
         bool isduplicate = false;
-        
+        for (int k = 0; k < i; k++){
+            if (arr[i] == arr[k]){
+                isduplicate = true;
+                break;
+            }
+        }
+        if(isduplicate)
+          continue;
+          int count = 1;
+          for(int j = i+1; j < n; j++){
+            if (arr[i] == arr[j])
+            count++;
+          }
+          if(count > 1)
+            cout << arr[i] << " ";
+
     }   
-     
+    return 0;
+}
