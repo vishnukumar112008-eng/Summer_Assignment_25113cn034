e7b2b4dd38c323a7523adc1b1613d3223396d5cf
Vishnu kumar <vishnukumar112008@gmail.com>
2026-06-15 12:35:16 +0530

    new

---
+++ Day-15/second.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin >> n;
+    int arr[n];
+    
+    for(int i = 0; i < n; i++)
+       cin >> arr[i];
+       int first = arr[0];
+       for(int i = 0 ; i < n-1; i++)
+       arr[i] = arr[i +1];
+
+       arr[n-1] = first;
+       cout << "array after left rotation: ";
+       for(int i = 0; i < n; i++)
+         cout << arr[i] << " ";
+         return 0;
+}
