52ba7d43f2b486fe087376ca3e449a70d2c01f89
Vishnu kumar <vishnukumar112008@gmail.com>
2026-06-16 20:06:05 +0530

    new

---
+++ Day-16/first.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout << "enter n: ";
+    cin >> n;
+    int arr[n-1];
+    cout << " enter " << n -1 << " element: ";
+    int sum = 0;
+    for ( int i = 0 ; i < n-1; i++){
+        cin >> arr [i];
+        sum += arr[i];
+
+    }
+    int total = n * (n+1)/2 ;
+    cout << "missing number = " << total - sum;
+    return 0;
+    
+ 
+}
